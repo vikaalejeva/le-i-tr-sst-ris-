be3342777aec512586e96868bf39640e70a4fd9a
vikaalejeva <78194978+vikaalejeva@users.noreply.github.com>
2021-12-03 12:20:44 +0000

    commint and push

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-#Uzrakstit programmu, kas pieprasa no lietotāja ievadīt trīs leņķis (kas varētu piederēt vienam trīsstūrim) un ar funkcijas palīdzību pārbauda, vai šie leņķi ir derīgī trīsstūra veidošanai.
-def lenkuParbaude(len1,len2,len3):
- rezultats = False
- if len1+len2+len3==180:
-  rezultats = True
- return rezultats
+#Izveidot funkciju, kas izvada saraksta elementus. Funkcijas parametri- elementu daudzums, saraksts.
+def izdruka(daudzums, sar1):
+ for elem in range(0,daudzums):
+  print(sar1[elem])
+ return 0
 
-len1 = int(input("Ievadiet 1.leņķi:"))
-len2 = int(input("Ievadiet 2.leņķi:"))
-len3 = int(input("Ievadiet 3.leņķi:"))
-rez = lenkuParbaude(len1,len2,len3)
-if rez:
- print("Trīsstūris eksistē!")
-else:
- print("Trīsstūris neeksistē!")
+saraksts = [2,4,5,6,1,2,34,5]
+daudzums = int(input("Ievadi elementu skaitu: "))
+rez = izdruka(daudzums, saraksts)
